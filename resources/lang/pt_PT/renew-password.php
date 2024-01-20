c78204937acaea87ea19cc2d99276278f8797314
Hélder Teixeira <corvisierpt@gmail.com>
2024-01-20 11:01:32 +0000

    European Portuguese Translation
    European Portuguese Translation

---
+++ resources/lang/pt_PT/renew-password.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => 'Actualizar palavra-passe',
+    'heading' => 'É necessário actualizar a sua palavra-passe de forma a aumentar a segurança da sua conta.',
+    'form' => [
+        'current-password' => [
+            'label' => 'Palavra-passe actual'
+        ],
+        'password' => [
+            'label' => 'Nova palavra-passe'
+        ],
+        'password-confirmation' => [
+            'label' => 'Confirmação de nova palavra-passe'
+        ],
+        'actions' => [
+            'renew' => [
+                'label' => 'Actualizar'
+            ]
+        ]
+    ],
+    'notifications' => [
+        'title' => 'Actualização de palavra-passe',
+        'body' => 'Palavra-passe actualizada com sucesso!'
+    ]
+];
