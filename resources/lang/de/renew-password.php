52844e2337ef188c044dc2987183646c59d85ec5
Daniel Elskamp <47042042+DGINXREAL@users.noreply.github.com>
2024-06-14 07:22:27 +0200

    feat: add german language (#12)

---
+++ resources/lang/de/renew-password.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => 'Passwort erneuern',
+    'heading' => 'Es ist Zeit ein neues Passwort zu setzen, zur deiner Sicherheit.',
+    'form' => [
+        'current-password' => [
+            'label' => 'Aktuelles Passwort'
+        ],
+        'password' => [
+            'label' => 'Neues Passwort'
+        ],
+        'password-confirmation' => [
+            'label' => 'Neues Passwort bestätigen'
+        ],
+        'actions' => [
+            'renew' => [
+                'label' => 'Erneuern'
+            ]
+        ]
+    ],
+    'notifications' => [
+        'title' => 'Passwort erneuert',
+        'body' => 'Erfolgreich!'
+    ]
+];
