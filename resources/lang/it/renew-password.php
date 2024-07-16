9730c24cd1073b7d6693078c12f9fa52358be153
Luca Longo <masterix21@gmail.com>
2024-07-16 07:10:50 +0200

    Adding Italian translation (#14)
    Thanks to @masterix21

---
+++ resources/lang/it/renew-password.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => 'Cambia la password',
+    'heading' => 'Per motivi di sicurezza, è opportuno aggiornare la tua password.',
+    'form' => [
+        'current-password' => [
+            'label' => 'Password attuale'
+        ],
+        'password' => [
+            'label' => 'Nuova password'
+        ],
+        'password-confirmation' => [
+            'label' => 'Conferma nuova password'
+        ],
+        'actions' => [
+            'renew' => [
+                'label' => 'Aggiorna'
+            ]
+        ]
+    ],
+    'notifications' => [
+        'title' => 'Ottimo!',
+        'body' => 'La tua password è stata aggiornata con successo.'
+    ]
+];
