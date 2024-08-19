1e3e234e13bb91a0d79e965f5486f476a2b83e9c
Eugen - Lucian Pașca <pasca.eugen@gmail.com>
2024-08-19 15:37:20 +0000

    added Ro lang

---
+++ resources/lang/ro/renew-password.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => 'Reînnoire parola',
+    'heading' => 'Este timpul să reînnoiești parola pentru a asigura securitatea contului tău.',
+    'form' => [
+        'current-password' => [
+            'label' => 'Parola actuală'
+        ],
+        'password' => [
+            'label' => 'Parola nouă'
+        ],
+        'password-confirmation' => [
+            'label' => 'Confirmă parola nouă'
+        ],
+        'actions' => [
+            'renew' => [
+                'label' => 'Reînnoiește'
+            ]
+        ]
+    ],
+    'notifications' => [
+        'title' => 'Reînnoire Parola',
+        'body' => 'Parola a fost reînnoită cu succes!'
+    ]
+];
