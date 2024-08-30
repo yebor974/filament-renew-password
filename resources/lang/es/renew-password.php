1d186b3c73f69678b88cfcd92be11e6b44bcc8e7
Coder Dev <30912173+coderdev21@users.noreply.github.com>
2024-08-30 01:36:25 -0500

    spanish language
    Add spanish language for translator

---
+++ resources/lang/es/renew-password.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => 'Cambiar Contraseña',
+    'heading' => 'Es hora de renovar su contraseña para mejorar la seguridad de su cuenta.',
+    'form' => [
+        'current-password' => [
+            'label' => 'Contraseña Actual'
+        ],
+        'password' => [
+            'label' => 'Nueva Contraseña'
+        ],
+        'password-confirmation' => [
+            'label' => 'Confirmar la nueva contraseña'
+        ],
+        'actions' => [
+            'renew' => [
+                'label' => 'Guardar'
+            ]
+        ]
+    ],
+    'notifications' => [
+        'title' => 'Cambiar Contraseña',
+        'body' => 'La contraseña de cambio exitosamente!'
+    ]
+];
