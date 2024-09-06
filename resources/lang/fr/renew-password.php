f9040983ca777779107d13d12b62694be4b747db
agencetwogether <53862310+agencetwogether@users.noreply.github.com>
2024-09-06 12:13:48 +0200

    French translation

---
+++ resources/lang/fr/renew-password.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => 'Renouveler le mot de passe',
+    'heading' => 'Pour renforcer la sécurité de votre compte, vous devez renouveler votre mot de passe.',
+    'form' => [
+        'current-password' => [
+            'label' => 'Mot de passe actuel'
+        ],
+        'password' => [
+            'label' => 'Nouveau mot de passe'
+        ],
+        'password-confirmation' => [
+            'label' => 'Confirmation du nouveau mot de passe'
+        ],
+        'actions' => [
+            'renew' => [
+                'label' => 'Renouveler'
+            ]
+        ]
+    ],
+    'notifications' => [
+        'title' => 'Renouvellement du mot de passe',
+        'body' => 'Réussi !'
+    ]
+];
